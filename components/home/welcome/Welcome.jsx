6e684cbf99a8053ab8018a15c1bc506a5e8bd3c6
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-12 13:39:53 +0300

    nearbyjobs ve popularjobs bölümleri eklendi.

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -50,9 +50,13 @@ const Welcome = () => {
                         router.push(`search/${item}`)
                     }}
                 >
-                    <Text>{item}</Text>
+                    <Text style={styles.tabText(activeJobType,item)}>{item}</Text>
                 </TouchableOpacity>
-            )} />
+            )}
+                      keyExtractor={item=>item}
+                      contentContainerStyle={{columnGap:SIZES.small}}
+                      horizontal
+            />
         </View>
     </View>
   )
