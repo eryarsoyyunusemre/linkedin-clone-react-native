703bc9c4340b624aafb345f0b02841e327c8f606
eryarsoyyunusemre <eryarsoyyunusemre@gmail.com>
2023-08-14 11:51:30 +0300

    job details içi düzenlendi.

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -3,9 +3,11 @@ import {Stack,useRouter} from "expo-router";
 import {useCallback, useState} from "react";
 import { useRoute } from '@react-navigation/native';
 
-import {Company,JobAbout,JobFooter,JobTabs,ScreenHeaderBtn} from "../../components";
+import {Company, JobAbout, JobFooter, JobTabs, ScreenHeaderBtn, Specifics} from "../../components";
 import {COLORS,icons,SIZES} from "../../constants";
 import useFetch from "../../hook/useFetch";
+
+const tabs = ["About","Qualifications","Responsibilities"]
 
 const JobDetails = () => {
     const route = useRoute();
@@ -17,11 +19,77 @@ const JobDetails = () => {
         job_id:params.id
     })
 
+    const [refreshing, setRefreshing] = useState();
+    const [activeTab, setActiveTab] = useState(tabs[0]);
+    const onRefresh = ()=>{}
+
+    const displayTabContent = () =>{
+        switch (activeTab) {
+            case "Qualifications":
+                return <Specifics
+                title="Qualifications"
+                points = {data[0].job_highlights?.qualifications ?? ['N/A']}
+                />
+            case "About":
+                break;
+            case "Responsibilities":
+                break;
+        }
+    }
     return (
         <SafeAreaView style={{flex:1,backgroundColor:COLORS.lightWhite}}>
-            <Stack.Screen>
+            <Stack.Screen
+            options={{
+                headerStyle:{backgroundColor:COLORS.lightWhite},
+                headerShadowVisible:false,
+                headerBackVisible:false,
+                // geri dönme buttonu
+                headerLeft: ()=>(
+                    <ScreenHeaderBtn
+                    iconUrl={icons.left}
+                    dimension='60%'
+                    handlePress={()=>router.back()}
+                    />
+                ),
+                // paylaşma buttonu
+                headerRight: ()=>(
+                    <ScreenHeaderBtn
+                        iconUrl={icons.share}
+                        dimension='60%'
+                    />
+                ),
+                headerTitle:''
+            }}/>
+            <>
+            <ScrollView showsVerticalScrollIndicator={false} refreshControl={
+                <RefreshControl refreshing={refreshing} onRefresh={onRefresh}/>
+            }>
+                {isLoading ? (
+                    <ActivityIndicator size="large" color={COLORS.primary}/>
+                ):error ? (
+                    <Text>Something went wrong</Text>
+                ): data.length === 0 ? (
+                    <Text>No data</Text>
+                ):(
+                    <View style={{padding:SIZES.medium,paddingBottom:100}}>
+                        <Company
+                            companyLogo ={data[0].employer_logo}
+                            jobTitle ={data[0].job_title}
+                            companyName ={data[0].employer_name}
+                            Location ={data[0].job_country}
+                        />
 
-            </Stack.Screen>
+                        <JobTabs
+                            tabs={tabs}
+                            activeTab={activeTab}
+                            setActiveTab={setActiveTab}
+                        />
+
+                        {displayTabContent()}
+                    </View>
+                )}
+            </ScrollView>
+            </>
         </SafeAreaView>
     )
 }
